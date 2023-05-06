58475561574c5d077161b061ba83f96a040ea880
dinarSoria <soriadinar93@gmail.com>
2023-05-06 15:10:35 -0300

    tronco

---
+++ go.mod
@@ -0,0 +1,49 @@
+module ahorasi
+
+go 1.20
+
+require github.com/elastic/go-elasticsearch/v8 v8.7.1
+
+require (
+	fyne.io/fyne v1.4.3
+	fyne.io/fyne/v2 v2.3.4 // indirect
+	fyne.io/systray v1.10.1-0.20230403195833-7dc3c09283d6 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/fredbi/uri v0.1.0 // indirect
+	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/fyne-io/gl-js v0.0.0-20220119005834-d2da28d9ccfe // indirect
+	github.com/fyne-io/glfw-js v0.0.0-20220120001248-ee7290d23504 // indirect
+	github.com/fyne-io/image v0.0.0-20220602074514-4956b0afb3d2 // indirect
+	github.com/go-gl/gl v0.0.0-20211210172815-726fda9656d6 // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20221017161538-93cebf72946b // indirect
+	github.com/go-text/typesetting v0.0.0-20230405155246-bf9c697c6e16 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/goki/freetype v0.0.0-20220119013949-7a161fd3728c // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/gopherjs/gopherjs v1.17.2 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/jsummers/gobmp v0.0.0-20151104160322-e2ba15ffa76e // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/srwiley/oksvg v0.0.0-20220731023508-a61f04f16b76 // indirect
+	github.com/srwiley/rasterx v0.0.0-20210519020934-456a8d69b780 // indirect
+	github.com/stretchr/testify v1.8.0 // indirect
+	github.com/tevino/abool v1.2.0 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.1 // indirect
+	github.com/xdg-go/stringprep v1.0.3 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	github.com/yuin/goldmark v1.4.13 // indirect
+	go.mongodb.org/mongo-driver v1.11.6 // indirect
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	golang.org/x/image v0.3.0 // indirect
+	golang.org/x/mobile v0.0.0-20211207041440-4e6c2922fdee // indirect
+	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
+	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
+	golang.org/x/text v0.6.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	honnef.co/go/js/dom v0.0.0-20210725211120-f030747120f2 // indirect
+)
---
+++ main.go
@@ -0,0 +1,161 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	//"github.com/elastic/go-elasticsearch/v8/typedapi/types/enums/emailpriority"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+
+	//"golang.org/x/text/search"
+
+	//"fyne.io/fyne/v2/internal/widget"
+	"fyne.io/fyne/v2/widget"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Person struct {
+	Name          string   `bson:"name"`
+	Lastname      string   `bson:"lastname"`
+	Email         string   `bson:"email"`
+	Musicalgenres []string `bson:"Musicalgenres"`
+}
+
+func main() {
+	// Set the URI of your MongoDB Atlas cluster
+	uri := "mongodb+srv://SdinarNetlabs:123dinar@cluster0.2ss0vbw.mongodb.net/?retryWrites=true&w=majority"
+
+	// Set client options
+	clientOptions := options.Client().ApplyURI(uri)
+
+	// Connect to MongoDB Atlas
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		panic(err)
+	}
+
+	// Check the connection
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connected to MongoDB Atlas!")
+
+	DataB := client.Database("DataB")
+	Collection := DataB.Collection("Persons")
+
+	app := app.New()
+	window := app.NewWindow("Menu")
+
+	nameEntry := widget.NewEntry()
+	nameLabel := widget.NewLabel("Nombre:")
+
+	lastnameEntry := widget.NewEntry()
+	lastnameLabel := widget.NewLabel("Apellido:")
+
+	emailEntry := widget.NewEntry()
+	emailLabel := widget.NewLabel("Email:")
+
+	genresEntry := widget.NewEntry()
+	genresLabel := widget.NewLabel("Géneros musicales favoritos (separados por coma):")
+
+	msLabel := widget.NewLabel("Ya existe un usuario registrado con ese email.")
+	msLabel.Hide()
+
+	resultsLabel := widget.NewLabel("Results")
+
+	searchButton := widget.NewButton("Buscar", func() {
+		var p Person
+
+		p.Musicalgenres = strings.Split(genresEntry.Text, ",")
+		filter := bson.M{"Musicalgenres": bson.M{"$in": p.Musicalgenres}}
+
+		cur, err := Collection.Find(context.Background(), filter)
+		if err != nil {
+			panic(err)
+		}
+		defer cur.Close(context.Background())
+
+		var results []Person
+		for cur.Next(context.Background()) {
+			var result Person
+			err := cur.Decode(&result)
+			if err != nil {
+				panic(err)
+			}
+			results = append(results, result)
+		}
+
+		if len(results) == 0 {
+			fmt.Println("No se encontraron resultados.")
+			return
+		}
+
+		var resultString string
+		resultString += "Resultados:\n"
+		for _, r := range results {
+			resultString += fmt.Sprintf("%s %s - %s\n", r.Name, r.Lastname, r.Email)
+		}
+
+		resultsLabel.SetText(resultString)
+	})
+	searchButton.Hide()
+
+	StartButton := widget.NewButton("Start", func() {
+		var p Person
+
+		p.Name = nameEntry.Text
+		p.Lastname = lastnameEntry.Text
+		p.Email = emailEntry.Text
+		p.Musicalgenres = strings.Split(genresEntry.Text, ",")
+
+		// Check if a person with the same email already exists
+		filter := bson.M{"email": p.Email}
+		existingPerson := Collection.FindOne(context.Background(), filter)
+		var per Person
+		if err := existingPerson.Decode(&per); err == nil {
+			// An object with the same email already exists, inform the user
+			msLabel.Show()
+			return
+		}
+
+		result, err := Collection.InsertOne(context.Background(), p)
+		if err != nil {
+			panic(err)
+		}
+		searchButton.Show()
+		fmt.Println("Inserted document with ID:", result.InsertedID)
+		widget.NewLabel(fmt.Sprintf("Usuario registrado con ID %v", result.InsertedID)).Show()
+
+	})
+
+	content := container.NewVBox(
+		container.NewHBox(nameLabel, nameEntry),
+		container.NewHBox(lastnameLabel, lastnameEntry),
+		container.NewHBox(genresLabel, genresEntry),
+		container.NewHBox(emailLabel, emailEntry),
+		StartButton,
+		msLabel,
+		searchButton,
+		resultsLabel,
+	)
+
+	window.SetContent(content)
+	window.ShowAndRun()
+
+	//fmt.Print("Ingrese sus generos musicales separados por comas: ")
+	//var genres string
+	//fmt.Scan(&genres)
+	//p.musicalgenres = strings.Split(genres, ",")
+
+}
